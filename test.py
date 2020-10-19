03191062f140a9474d71f1bfc2786d80a660239c
Ian Dulchinos <ian.dulchinos@gmail.com>
2020-10-19 14:34:47 -0700

    getting better performance, app is currently slightly broken, will fix later

--- test.py
+++ test.py
@@ -4,59 +4,72 @@ from pygame.locals import * # for mouse stuff
 import time
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import os
+
+cl = np.clongdouble # precision difficulties...
+THRESHOLD = 4.0
 
 class MandelViewer:
     def __init__(self, W, H):
         self.W = W
         self.H = H
         self.size = (W, H)
-        self.THRESHOLD = 2
-        self.xRange = 4.0 # just used for initialization
-        self.yRange = 4.0 # same with this
-        self.tl = -2.0 + 2.0j # and this
-        self.reals = np.linspace(self.tl.real + 0j, self.tl.real + self.xRange + 0j, W)
-        self.imags = np.linspace(0 + 1.0j * self.tl.imag, 0 + 1.0j * (self.tl.imag - self.yRange), H)
+        self.reals = np.linspace(-2.0 + 0j, 2.0 + 0j, W)
+        self.imags = np.linspace(0 + 2.0j, 0 + -2.0j, H)
         self.region = self.reals[:, np.newaxis] + self.imags
-        self.MAX_IT = 200
-        self.z = self.region.copy()
+        self.MAX_IT = 64
+        self.MAX_PRECISION = 64 * 8
         # color stuff, basically color dict maps each 0 < i <= MAX_IT to int32 val
         self.cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=self.MAX_IT), cmap=mpl.cm.jet)
-        self.color_dict = {j:self.rgba_to_int32(self.cmap.to_rgba(j)) for j in range(1, self.MAX_IT+1)}
-        self.pixels = (self.color_dict[self.MAX_IT]) * (np.ones(W * H).reshape(W, H))
-        self.i = 1
+        self.color_dict = [self.rgba_to_int32(self.cmap.to_rgba(j)) for j in range(self.MAX_IT)]
+        self.pixels = (self.color_dict[self.MAX_IT-1]) * (np.ones(W * H).reshape(W, H))
+        pygame.font.init()
+        self.font = pygame.font.Font('/Users/iandulchinos/Desktop/mandelbrot/mandelbrot/PressStart2P.ttf', 11)
+        self.zoom_factor = 1.0
 
+    # helper function to convert float rgba -> int32
     def rgba_to_int32(self, rgba):
         return sum((256 ** i) * int(rgba[(2 - i) % 4]*255) for i in range(4)) # rgba -> a r g b (thats why (2 - i) % 4)
 
-    def update_pixels(self, step):
-        if self.i >= self.MAX_IT:
-            return
-        for _ in range(step):
-            if self.i > self.MAX_IT:
-                break
-            else:
-                self.z = np.where(np.absolute(self.z) > self.THRESHOLD,
-                                  self.z,
-                                  self.z ** 2 + self.region)
-                self.pixels = np.where((np.absolute(self.z) > self.THRESHOLD) & (self.pixels == self.color_dict[self.MAX_IT]),
-                                       self.color_dict[self.i],
-                                       self.pixels)
-                self.i += 1
+    # this is where the math happens
+    def update_pixels(self):
+        z = self.region.copy()
+        for i in range(self.MAX_IT):
+            magsqr = z.imag*z.imag + z.real*z.real
+            self.z = np.where(magsqr > THRESHOLD,
+                              z,
+                              z ** 2 + self.region)
+            self.pixels = np.where((magsqr > THRESHOLD)
+                                        & (self.pixels == self.color_dict[self.MAX_IT-1]),
+                                   self.color_dict[i],
+                                   self.pixels)
+
 
     def zoom(self, anchor_x, anchor_y, scale=0.1): # negative scale -> zoom out
-        self.i = 1
         self.reals = np.add((1 - scale)*self.reals, scale * self.reals[anchor_x])
         self.imags = np.add((1 - scale)*self.imags, scale * self.imags[anchor_y])
         self.region = self.reals[:, np.newaxis] + self.imags
-        self.z = self.region.copy()
-        self.pixels = (self.color_dict[self.MAX_IT]) * (np.ones(self.W * self.H).reshape(self.W, self.H))
+
+    # def zoom2(self, anchor_x, anchor_y, scale=0.1):
+    #     self.i = 0
+    #
 
     def run(self):
         self.screen = pygame.display.set_mode(self.size)
 
         while True:
-            self.update_pixels(step=10)
+            start = time.time()
+            self.update_pixels()
+            time_to_update = round(time.time() - start, 5)
             pygame.surfarray.blit_array(self.screen, self.pixels) # blit_array must use numpy
+
+
+            update_time_text = self.font.render("Calculation Time: " + str(time_to_update) + 's', True, (255, 255, 255))
+            pos_str = "Location: " + str(self.region[self.H//2][self.W//2].real) + "  +  " + str(self.region[self.H//2][self.W//2].imag) + "i"
+            position_text = self.font.render(pos_str, True, (255, 255, 255))
+            self.screen.blit(update_time_text, (10, 10))
+            self.screen.blit(position_text, (10, 50))
+
             pygame.display.flip()
 
             for event in pygame.event.get():
@@ -66,7 +79,16 @@ class MandelViewer:
                 elif event.type == MOUSEWHEEL:
                     self.zoom(*pygame.mouse.get_pos(), scale=(0.1 * event.y))
                 elif event.type == pygame.KEYDOWN and event.mod == KMOD_NONE:
-                    print('key pressed')
+                    if event.key == pygame.K_UP:
+                        self.MAX_IT = self.MAX_IT + 64 if self.MAX_IT < self.MAX_PRECISION else self.MAX_PRECISION
+                        self.color_dict = [self.rgba_to_int32(self.cmap.to_rgba(j)) for j in range(self.MAX_IT)]
+                        self.i = 0
+                        print(self.MAX_IT)
+                    elif event.key == pygame.K_DOWN:
+                        self.MAX_IT = self.MAX_IT - 64 if self.MAX_IT > 0 else 0
+                        self.color_dict = [self.rgba_to_int32(self.cmap.to_rgba(j)) for j in range(self.MAX_IT)]
+                        self.i = 0
+                        print(self.MAX_IT)
 
 if __name__ == '__main__':
     M = MandelViewer(600, 600)
