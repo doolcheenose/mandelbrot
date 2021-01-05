1ea09b73cc749093b569a0475a007577b38c09fb
Ian Dulchinos <>
2021-01-05 15:03:10 -0800

    Cleaning up display and removing unnecessary files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mandelbrot
-interactive mandelbrot set viewer
+Interactive mandelbrot set viewer using python, pygame, and numpy.
+
+To run: download files and run `python main.py` or `python3 main.py` in root folder. Scroll in/out to zoom, and press up/down keys to increase/decrease resolution, at the cost of performance.
+
+GIF:
+![](mandelbrot_example.gif)
+
+
--- main.py
+++ main.py
@@ -1,100 +1,103 @@
 import numpy as np
-import pyglet as pg
-from pyglet.gl import *
-from OpenGL.GLUT import *
+import pygame
+from pygame.locals import * # for mouse stuff
+import time
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import time
+import os
 
+cl = np.clongdouble # precision difficulties...
+THRESHOLD = 4.0
 
-THRESHOLD = 2 # pre-chosen
-topleft = (-2.0, 2.0)
-xRange = 4.0
-yRange = 4.0
-W = 600
-H = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARK_GREY = (70, 70, 70)
+LIGHT_GREY = (180, 180, 180)
 
-# use broadcasting to create the screen
-region = np.linspace(topleft[0] + 0j,
-    topleft[0]+xRange + 0j, 2*W)[:, np.newaxis] + np.linspace(0 + 1.0j*topleft[1],
-    0 + 1.0j*(topleft[1]-yRange), 2*H)
-
-print(region.shape)
-
-def iterate(numIterations):
-    z = region.copy()
-    reg = region.copy()
-    pixels = numIterations * np.ones(4 * W * H).reshape(region.shape)
-    for i in range(1, numIterations):
-        z = np.where(np.absolute(z) < THRESHOLD, z ** 2 + reg, z)
-        pixels = np.where((pixels == numIterations) & (np.absolute(z) > THRESHOLD), i, pixels)
-    return pixels
-
-class M_Window(pg.window.Window):
-    def __init__(self, W, H, precision):
-        super(M_Window, self).__init__(W, H)
-        self.batch = pg.graphics.Batch()
+class MandelViewer:
+    def __init__(self, W, H):
         self.W = W
         self.H = H
-        self.THRESHOLD = 2
-        self.xRange = 4.0
-        self.yRange = 4.0
-        self.tl = -2.0 + 2.0j
-        self.region = np.linspace(self.tl.real + 0j,
-            self.tl.real+self.xRange + 0j, 2*W)[:, np.newaxis] + np.linspace(0 + 1.0j*self.tl.imag,
-            0 + 1.0j*(self.tl.imag-self.yRange), 2*H)
-        self.pixels = precision * np.ones(4 * W * H).reshape(self.region.shape)
-        self.z = self.region.copy()
-        self.reg = self.region.copy() # bad code, might be unnecessary
-        self.i = 1
-        self.NUM_IT = precision
-        self.cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=self.NUM_IT), cmap=mpl.cm.jet)
+        self.size = (W, H)
+        self.reals = np.linspace(-2.0 + 0j, 2.0 + 0j, W)
+        self.imags = np.linspace(0 + 2.0j, 0 + -2.0j, H)
+        self.region = self.reals[:, np.newaxis] + self.imags
+        self.MAX_IT = 64
+        self.IT_INC = 64
+        # color stuff, basically color dict maps each 0 < i <= MAX_IT to int32 val
+        self.cmap = mpl.cm.ScalarMappable(norm=mpl.colors.Normalize(vmin=1, vmax=self.IT_INC), cmap=mpl.cm.jet)
+        self.color_dict = [self.rgba_to_int32(self.cmap.to_rgba(j+1)) for j in range(self.IT_INC)]
+        self.pixels = (self.color_dict[self.IT_INC-1]) * (np.ones(W * H).reshape(W, H))
+        pygame.font.init()
+        self.font = pygame.font.Font('./fonts/PressStart2P.ttf', 8)
+        self.zoom_factor = 1.0
 
-    # called every 1/10.0 seconds, update 10 frames
-    def update_region(self):
-        if self.i < self.NUM_IT:
-            start = time.time()
-            for _ in range(10): # do 10 iterations every update, can be customizable
-                self.z = np.where(np.absolute(self.z) < self.THRESHOLD, self.z ** 2 + self.reg, self.z)
-                self.pixels = np.where((self.pixels == self.NUM_IT) & (np.absolute(self.z) > self.THRESHOLD), self.i, self.pixels)
-                self.i = self.i + 1
-            print('calculations took {0} seconds'.format(time.time() - start))
+    # helper function to convert float rgba -> int32
+    def rgba_to_int32(self, rgba):
+        return sum((256 ** i) * int(rgba[(2 - i) % 4]*255) for i in range(4)) # rgba -> a r g b (thats why (2 - i) % 4)
 
-    def temp_update(self):
+    # this is where the math happens
+    def update_pixels(self):
+        z = self.region.copy()
+        self.pixels = (self.color_dict[(self.MAX_IT-1) % 64]) * (np.ones(self.W * self.H).reshape(self.W, self.H))
+        for i in range(self.MAX_IT):
+            magsqr = z.imag*z.imag + z.real*z.real
+            z = np.where(magsqr > THRESHOLD,
+                              z,
+                              z * z + self.region)
+            self.pixels = np.where((magsqr > THRESHOLD)
+                                        & (self.pixels == self.color_dict[(self.MAX_IT-1) % 64]),
+                                   self.color_dict[i % 64],
+                                   self.pixels)
 
 
 
-    def update_frame(self, x, y):
-        start = time.time()
-        self.update_region()
-        self.color_pixels = self.cmap.to_rgba(self.pixels).flatten()
-        print('update_frame took {0} seconds'.format(time.time() - start))
+    def zoom(self, anchor_x, anchor_y, scale=0.1): # negative scale -> zoom out
+        self.reals = np.add((1 - scale)*self.reals, scale * self.reals[anchor_x])
+        self.imags = np.add((1 - scale)*self.imags, scale * self.imags[anchor_y])
+        self.region = self.reals[:, np.newaxis] + self.imags
 
-    """
-    Turns out, using the rgba format with float values is wildly inneficient,
-    taking ~1.5s to just draw the image to the screen, by far the biggest bottleneck
-    in the system. Working on using a more efficient format, may just map manually
-    without using the cmap feature of matplotlib
-
-    apparently, glDrawPixels will always be slow, and i should use textures to
-    accomplish this task. I will try the first fix in the paragraph above to see
-    if the problem persists, and if so i will use textures or try a different
-    route. It seems as though i should be able to accomplish this without
-    directly accessing the openGL code, and just use pyglet. We will see...
-    """
-    # def on_draw(self):
+    # def zoom2(self, anchor_x, anchor_y, scale=0.1):
+    #     self.i = 0
     #
-    #     start = time.time()
-    #     glClear(GL_COLOR_BUFFER_BIT)
-    #     glDrawPixels(2 * self.W, 2 * self.H, GL_RGBA, GL_FLOAT, (GLfloat * len(self.color_pixels))(*self.color_pixels))
-    #     glutSwapBuffers()
-    #     print('on_draw took {0} seconds'.format(time.time() - start))
-    def on_draw(self): # temporary test
-        self.batch.draw()
 
+    def run(self):
+        self.screen = pygame.display.set_mode(self.size)
+
+        while True:
+            start = time.time()
+            self.update_pixels()
+            time_to_update = round(time.time() - start, 5)
+            pygame.surfarray.blit_array(self.screen, self.pixels) # blit_array must use numpy
+
+
+            update_time_text = self.font.render("Calculation Time: " + str(time_to_update) + 's', True, LIGHT_GREY)
+            pos_str = "Location: " + str(self.region[self.H//2][self.W//2].real) + "  +  " + str(self.region[self.H//2][self.W//2].imag) + "i"
+            iteration_str = 'Iterations: {}'.format(self.MAX_IT)
+            position_text = self.font.render(pos_str, True, LIGHT_GREY)
+            iteration_text = self.font.render(iteration_str, True, LIGHT_GREY)
+            
+            pygame.draw.rect(self.screen, BLACK, pygame.Rect(5, 5, 500, 35))
+            self.screen.blit(update_time_text, (10, 10))
+            self.screen.blit(position_text, (10, 30))
+            self.screen.blit(iteration_text, (300, 10))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                elif event.type == MOUSEWHEEL:
+                    self.zoom(*pygame.mouse.get_pos(), scale=(0.1 * event.y))
+                elif event.type == pygame.KEYDOWN and event.mod == KMOD_NONE:
+                    if event.key == pygame.K_UP:
+                        self.MAX_IT = self.MAX_IT + self.IT_INC
+                        print(self.MAX_IT)
+                    elif event.key == pygame.K_DOWN:
+                        self.MAX_IT = self.MAX_IT - self.IT_INC if self.MAX_IT > 0 else 0
+                        print(self.MAX_IT)
 
 if __name__ == '__main__':
-    window = M_Window(W, H, precision=200)
-
-    pyglet.clock.schedule(window.update_frame, 1/10.0)
-    pyglet.app.run()
+    M = MandelViewer(600, 600)
+    M.run()
